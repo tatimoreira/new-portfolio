394ac9c0e348cf2cd7b0a7a98a09dfae35d14b7f
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2024-03-14 20:42:08 -0600

    fix: external links

--- app/components/navigation/Navbar.tsx
+++ app/components/navigation/Navbar.tsx
@@ -1,6 +1,7 @@
 import { ReactNode } from "react";
 import type { IconType } from "react-icons";
 import { NavLink } from "@remix-run/react";
+import { redirect } from "@remix-run/node";
 
 interface NavbarProps {
   links: Array<{
@@ -16,23 +17,30 @@ export default function Navbar({ links }: NavbarProps) {
   return (
     <header className=" flex w-full rounded-2xl  bg-teal-500 p-4 text-white sm:flex sm:items-center sm:justify-center sm:rounded-2xl">
       {links.map(({ to, label, icon: Icon, external }, idx) => (
-        <NavLink
-          to={to}
-          key={to}
-          target={external ? "_blank" : ""}
-          rel="noreferrer"
-          className={({ isActive }) => (isActive ? "active" : "pending")}
-          style={({ isActive }) => {
-            return {
-              marginLeft: idx === 0 ? undefined : " 2rem ",
-              fontWeight: isActive ? "bold " : "",
-              color: isActive ? "rgb(253 186 116)" : "white",
-            };
-          }}
-        >
-          <Icon className="hover:fill-orange-300  " />
-        </NavLink>
+
+        external
+          ?
+          (<a href={to} target="_blank" className="ml-8">  <Icon className="hover:fill-orange-300  " /></a>) : (
+            <NavLink
+              to={to}
+              key={to}
+              target={external ? "_blank" : ""}
+              rel="noreferrer"
+              className={({ isActive }) => (isActive ? "active" : "pending")}
+              style={({ isActive }) => {
+                return {
+                  marginLeft: idx === 0 ? undefined : " 2rem ",
+                  fontWeight: isActive ? "bold " : "",
+                  color: isActive ? "rgb(253 186 116)" : "white",
+                };
+              }}
+            >
+              <Icon className="hover:fill-orange-300  " />
+            </NavLink>
+          )
+
+
       ))}
-    </header>
+    </header >
   );
 }
